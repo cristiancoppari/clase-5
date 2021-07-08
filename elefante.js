a71d75a2f6ddb90deb81aa8e01bbb1ae9482e6b4
Andrés Pérez <juanandresperezpt@gmail.com>
2021-07-08 17:48:16 -0500

    Nueva información sobre nuestro elefante

--- elefante.js
+++ elefante.js
@@ -1,1 +1,7 @@
-console.log("Hola soy un Elefante")
+console.log("Hola soy un Elefante");
+
+partes = ['cola', 'oreja', 'pata'];
+
+partes.forEach(parte => {
+    console.log(`El elefante contiene en su cuerpo la parte de ${parte}`)
+});
